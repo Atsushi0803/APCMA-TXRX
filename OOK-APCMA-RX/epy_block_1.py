1a76e5614fea3af19bd37ec6c8a275dd00341a2f
Atsushi0803 <atsushi.7624@gmail.com>
2021-11-08 13:48:27 +0900

    コードの整理& GUIの削除

--- OOK-APCMA-RX/epy_block_1.py
+++ OOK-APCMA-RX/epy_block_1.py
@@ -3,7 +3,7 @@ from gnuradio import gr
 
 
 class PulseDetection(gr.decim_block):
-    def __init__(self, Sub_slot_rate=5):
+    def __init__(self, Sub_slot_rate=8):
         gr.decim_block.__init__(
             self,
             name='Pulse Detection',
@@ -14,6 +14,5 @@ class PulseDetection(gr.decim_block):
         self.set_relative_rate(1.0/Sub_slot_rate)
 
     def work(self, input_items, output_items):
-        output_items[0][:] = [(sum(input_items[0][self.decim * i : self.decim * (i + 1)]) > 0.5 * self.decim) for i in
-                              range(len(output_items[0][:]))]
+        output_items[0][:] = [sum(input_items[0][j+self.decim*i] > 0.5 for j in range(self.decim)) > self.decim * 0.5 for i in range(len(output_items[0][:]))]
         return len(output_items[0][:])
--- OOK-APCMA-RX/epy_block_2.py
+++ OOK-APCMA-RX/epy_block_2.py
@@ -1,9 +1,10 @@
+# -*- coding: utf-8 -*-
 import numpy as np
 from gnuradio import gr
 
 
 class ApcmaDecode(gr.sync_block):
-    def __init__(self, B=2):
+    def __init__(self, bits_per_symbol=4):
         gr.sync_block.__init__(
             self,
             name='Decoding APCMA',
@@ -11,25 +12,24 @@ class ApcmaDecode(gr.sync_block):
             out_sig=None
         )
 
-        # 復号するために参照するパルス列の作成
-        self.B = B  # [bits/symbol]
-        self.number_of_slot = 2 ** (self.B + 1) + 5
-        self.ApcmaCode = [[0] * self.number_of_slot for i in range(2 ** self.B)]
-        for i in range(2 ** self.B):
+        self.bits_per_symbol = bits_per_symbol  # [bits/symbol]
+        self.number_of_slot = 2 ** (self.bits_per_symbol + 1) + 5
+        self.ApcmaCode = [[0] * self.number_of_slot for i in range(2 ** self.bits_per_symbol)]
+        for i in range(2 ** self.bits_per_symbol):
             self.ApcmaCode[i][0] = 1
             self.ApcmaCode[i][i + 2] = 1
             self.ApcmaCode[i][- i - 3] = 1
             self.ApcmaCode[i][-1] = 1
 
-        self.PulseList = np.zeros(self.number_of_slot)  # 入力されたパルス列のリスト
+        self.PulseList = np.zeros(self.number_of_slot)
 
     def work(self, input_items, output_items):
         for i in range(len(input_items[0][:])):
             self.PulseList = np.append(self.PulseList[1:], input_items[0][i])
-            print(self.PulseList)
+            # print(self.PulseList)
             for j in range(len(self.ApcmaCode)):
-                if np.all(np.logical_and(self.PulseList, self.ApcmaCode[j]) == self.ApcmaCode[j]):
-                    print(f'Word:{j+1}')
-                    # with open('C:\\Users\\Atsushi.N\\Desktop\\Output.txt', 'a') as f:
-                    #     f.write(j+1)
+                if np.all(np.logical_and(self.ApcmaCode[j][:], self.PulseList) == self.ApcmaCode[j][:]):
+                    # print("Word:{}".format(j + 1))
+                    with open('C:\\Users\\Atsushi.N\Desktop\\Output.txt', 'a') as f:
+                        f.write(f'{j+1}\n')
         return 0
